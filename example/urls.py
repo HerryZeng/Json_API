411df8b7e11e58245bad9ab6da079ebdff3339ae
Herry <herry.zeng@gmail.com>
2018-01-18 21:29:15 +0800

    替换views.py文件和修改urls.py文件

--- example/urls.py
+++ example/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path,re_path
+from django.conf.urls import url
 from django.views.generic import TemplateView
+from . import views1
 from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='example/index.html')),
-    re_path('ajax/more/',view=views.more_poems),
+    # re_path('ajax/more/', view=views1.more_poems),
+    url(r'ajax/more/$', view=views.more_poems),
+    url(r'ajax/add/$', view=views.add),
 ]
